9dc7c04d8a95362a32d8df514f4a65a58e97008f
ABB <bayhanbayramoglu@gmail.com>
2020-04-26 12:16:45 +0300

    Fontawesome Bootstrap added. Builded  directory constructor.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="header">
+      <div class="logo"></div>
+      <div class="heading">Gladio Blog Admin Panel & Control Systems</div>
     </div>
-    <router-view/>
+    <div id="content">
+      <div id="sidemenu">
+        <nav>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </nav>
+      </div>
+      <div class="incontent">
+        <router-view />
+      </div>
+    </div>
+    <div id="footer">
+      <h3>Footer</h3>
+    </div>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+@import url("./assets/styles/bootstrap/css/bootstrap.css");
+@import url("./assets/styles/fontawesome/css/all.css");
+@import url("./assets/styles/style.css");
+@import url("./assets/styles/grid.css");
+@import url("./assets/styles/media.css");
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
